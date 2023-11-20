479e5b55e4d544377283a65de2edcb59aa80e3ae
Ashani Mae <flava4lyphe92@gmail.com>
2023-11-19 22:32:21 -0500

    Added world.js

---
+++ world.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const lookup = document.querySelectorAll("#lookup")
+    const country = document.getElementById("country")
+    const result = document.getElementById("result")
+
+    lookup.forEach(function (button){
+        button.addEventListener('click', function (e){
+
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    result.innerHTML=this.responseText;
+                }
+            };
+            
+            xmlhttp.open("GET", "world.php?country=" + country.value, true);
+            xmlhttp.send();
+        })
+    })
+
+
+    
+})
--- world.php
+++ world.php
@@ -1,15 +1,25 @@
 <?php
+// header("Access-Control-Allow-Origin: *");
+
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$country = isset($_GET ['country']) ? $_GET ['country'] : '';
+$stmt = $conn->prepare ("SELECT * FROM countries WHERE name LIKE :country");
+$stmt ->execute(['country' => '%' . $country . '%']);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($_SERVER[ 'REQUEST_METHOD'] == 'GET'){
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
 
 ?>
+
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
